51a32d1b3069e1d4121b5fd33e78ac2865076252
WhoInGodsName <61045310+WhoInGodsName@users.noreply.github.com>
2024-04-14 21:49:56 +0800

    Add project files.

---
+++ RoR2Cheat/Loader.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using RoR2;
+
+namespace RoR2Mod
+{ 
+    public class Loader : MonoBehaviour
+    {
+        public static void Init()
+        {
+            Loader.Load = new GameObject();
+            Loader.Load.AddComponent<RoRMod>();
+            UnityEngine.Object.DontDestroyOnLoad(Loader.Load);
+        }
+
+        public static void Unload()
+        {
+            _Unload();
+        }
+
+        private static void _Unload()
+        {
+            GameObject.Destroy(Load);
+        }
+
+        private static GameObject Load;
+    }
+}
---
+++ RoR2Cheat/RoR2Mod.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using RoR2;
+
+namespace RoR2Mod
+{
+
+    internal class RoRMod : MonoBehaviour
+    {
+        CharacterBody _Body = FindObjectOfType<CharacterBody>();
+
+        public void OnGUI()
+        {
+            GUI.Box(new Rect(0f, 0f, 100f, 30f), "");
+            GUI.Label(new Rect(0f, 0f, 100f, 30f), "Label.");
+        }
+        public void Start()
+        {
+
+            _Body = FindObjectOfType<CharacterBody>();
+        }
+
+        public void Update()
+        {
+            _Body.sprintingSpeedMultiplier = 50f;
+        }
+
+    }
+}
