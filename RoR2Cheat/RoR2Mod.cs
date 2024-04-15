07aecbc2c9ffbeb313d700dc5d30cbf05dfec708
WhoInGodsName <61045310+WhoInGodsName@users.noreply.github.com>
2024-04-15 10:23:06 +0800

    0.0.1 Base variables

--- RoR2Cheat/RoR2Mod.cs
+++ RoR2Cheat/RoR2Mod.cs
@@ -6,17 +6,25 @@ namespace RoR2Mod
     //Main cheat class
     internal class RoRMod : MonoBehaviour
     {
-        CharacterBody _Body = FindObjectOfType<CharacterBody>();
+        CharacterBody _Body;
+        CharacterMaster _Master;
+        NetworkUser _NetworkUser;
+        TeamManager _TeamManager;
+        TeleporterInteraction _Teleporter;
 
         public void OnGUI()
         {
-            GUI.Box(new Rect(0f, 0f, 100f, 30f), "");
-            GUI.Label(new Rect(0f, 0f, 100f, 30f), "Label.");
+            GUI.Box(new Rect(0f, 0f, 300f, 500f), "");
+            //GUI.Label(new Rect(0f, 0f, 100f, 30f), "Label.");
         }
         public void Start()
         {
 
             _Body = FindObjectOfType<CharacterBody>();
+            _Master = FindObjectOfType<CharacterMaster>();
+            _NetworkUser = FindObjectOfType<NetworkUser>();
+            _TeamManager = FindObjectOfType<TeamManager>();
+            _Teleporter = FindObjectOfType<TeleporterInteraction>();
         }
 
         public void Update()
